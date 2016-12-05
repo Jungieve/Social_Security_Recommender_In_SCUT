8cc47ae75b1f0a550964e137704408369a2ee6f3
Jungieve <zihan.cong@gmail.com>
2016-12-05 16:45:56 +0800

    增加mongodb的connection包

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/user_controller');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.get('/:userId',userController.get);
 module.exports = router;
