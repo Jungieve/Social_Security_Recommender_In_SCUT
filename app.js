8b8878321f76cd5aa9a10623f1cfe6d81462cfaf
Jungieve <zihan.cong@gmail.com>
2016-12-06 15:30:03 +0800

    app.js增加mongodb的连接require

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// connect mongodb using mongoose middleware
+require('./lib/connection');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -24,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
